beb4eeb30b430c7a0a27aa28efdd995355f1bee6
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-05-16 16:27:53 +0200

    add: domain model AuthUser v1 (no test)

---
+++ domain/model.py
@@ -0,0 +1,18 @@
+class AuthUser:
+    def __init__(self, user_id, username, password, superuser=False):
+        self.id = user_id
+        self.username = username
+        self.password = password
+        self.superuser = superuser
+
+    def __repr__(self):
+        return f"<AuthUser {self.id} {self.username}>"
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, AuthUser)
+            and self.id == other.id
+            and self.username == other.username
+            and self.password == other.password
+            and self.superuser == other.superuser
+        )
