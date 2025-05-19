980d84b89fc806d346487cd4d903a204a5c84c7a
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-05-19 12:16:19 +0200

    feature(green): implementation of the abstract repo, service layer may call public method implemented by children Repositories.

--- adapters/repositories.py
+++ adapters/repositories.py
@@ -1,45 +1,64 @@
 from abc import ABC, abstractmethod
-import domain.model as model
 
 
 class AbstractRepository(ABC):
-    @abstractmethod
+    def __init__(self):
+        # implement caching later to reduce redundant DB queries
+        self.cached = set()
+
     def add(self, model_obj):
-        raise NotImplementedError
+        self._add(model_obj)
 
-    @abstractmethod
-    def get(self, obj_id):
-        raise NotImplementedError
+    def get(self, model_cls, obj_pk):
+        return self._get(model_cls, obj_pk)
 
-    @abstractmethod
-    def delete(self, model_obj):
-        raise NotImplementedError
+    def delete(self, model_cls, obj_pk):
+        self._delete(model_cls, obj_pk)
 
-    @abstractmethod
-    def list(self):
-        raise NotImplementedError
+    def list(self, model_cls):
+        return self._list(model_cls)
 
-    @abstractmethod
     def update(self, model_obj):
+        self._update(model_obj)
+
+    @abstractmethod
+    def _add(self, model_obj):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get(self, model_cls, obj_pk):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _delete(self, model_cls, obj_pk):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _list(self, model_cls):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _update(self, model_obj):
         raise NotImplementedError
 
 
 class SqlAlchemyRepository(AbstractRepository):
     def __init__(self, session):
+        super().__init__()
         self.session = session
 
-    def add(self, model_obj):
+    def _add(self, model_obj):
         self.session.add(model_obj)
 
-    def get(self, obj_id):
-        return self.session.query(model.AuthUser).filter_by(id=obj_id).one()
+    def _get(self, model_cls, obj_pk):
+        return self.session.get(model_cls, obj_pk)
 
-    def delete(self, model_obj):
-        obj = self.session.merge(model_obj)
+    def _delete(self, model_cls, obj_pk):
+        obj = self.session.get(model_cls, obj_pk)
         self.session.delete(obj)
 
-    def list(self):
-        return self.session.query(model.AuthUser).all()
+    def _list(self, model_cls):
+        return self.session.query(model_cls).all()
 
-    def update(self, model_obj):
+    def _update(self, model_obj):
         self.session.merge(model_obj)
