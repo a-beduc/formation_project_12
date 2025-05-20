b4afa8792ff550ab9dfa7851f1e6247cb82ba84f
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-05-20 09:20:41 +0200

    test(red): addition of temporary test for the implementation of the uow pattern

---
+++ tests/test_service/mock_uow.py
@@ -0,0 +1,58 @@
+from adapters.repositories import AbstractRepository, AbstractUserRepository
+from domain.model import AuthUser
+from services.unit_of_work import AbstractUnitOfWork
+
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, init=()):
+        super().__init__()
+        self._store = {}
+        self._pk = 0
+        for obj in init:
+            self._add(obj)
+
+    def _add(self, model_obj):
+        if getattr(model_obj, "id", None) is None:
+            self._pk += 1
+            model_obj.id = self._pk
+        self._store[model_obj.id] = model_obj
+
+    def _get(self, obj_pk):
+        return self._store.get(obj_pk, None)
+
+    def _delete(self, obj_pk):
+        self._store.pop(obj_pk, None)
+
+    def _list(self):
+        return list(self._store.values())
+
+    def _update(self, model_obj):
+        if getattr(model_obj, "id", None) is None:
+            raise ValueError('The object id is not found')
+        self._store[model_obj.id] = model_obj
+
+
+class FakeUserRepository(FakeRepository, AbstractUserRepository):
+    def get_by_username(self, username):
+        return next(
+            (user for user in self._store.values() if
+             isinstance(user, AuthUser) and user.username == username), None
+        )
+
+
+class FakeUnitOfWork(AbstractUnitOfWork):
+    def __init__(self):
+        self.commited = False
+        self.users = FakeUserRepository()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    def rollback(self):
+        pass
+
+    def _commit(self):
+        self.commited = True
