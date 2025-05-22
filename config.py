b6171bc4e4e7167bb5a6d244f034e8bd89d90437
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-05-22 19:19:00 +0200

    side fix: several fixes +.env.example update + config retrieve data from .env for JWT + Password update in manual test tables (using argon2) + module PyJWT

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 
 load_dotenv()
@@ -12,3 +13,19 @@ def get_postgres_uri():
     port = os.getenv('PG_PORT')
     dbname = os.getenv('PG_DBNAME')
     return f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
+
+
+def get_secret_key():
+    return os.getenv('SECRET_KEY')
+
+
+def get_token_store_path():
+    return str(Path(__file__).resolve().parent / os.getenv('TOKEN_STORAGE'))
+
+
+def get_token_access_lifetime():
+    return int(os.getenv('ACCESS_LIFETIME'))
+
+
+def get_token_refresh_lifetime():
+    return int(os.getenv('REFRESH_LIFETIME'))
