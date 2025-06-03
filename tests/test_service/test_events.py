5cf98cef796f949c52a7d3c696309d8696d10867
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-06-03 20:51:34 +0200

    tests(red) : tests to implement business logic for event service

---
+++ tests/test_service/test_events.py
@@ -0,0 +1,108 @@
+import pytest
+from datetime import datetime
+
+from domain.model import Collaborator, Role, Contract, Event
+from tests.test_service.conftest import FakeRepository
+
+from services.app.events import EventService, EventServiceError
+
+
+@pytest.fixture
+def init_uow(uow):
+    coll_a = Collaborator(first_name="fn_a", last_name="ln_a",
+                          _role_id=Role.SALES, _user_id=1)
+    coll_b = Collaborator(first_name="fn_b", last_name="ln_b",
+                          _role_id=Role.SUPPORT, _user_id=2)
+    coll_c = Collaborator(first_name="fn_c", last_name="ln_c",
+                          _role_id=Role.MANAGEMENT, _user_id=3)
+    uow.collaborators = FakeRepository(init=(coll_a, coll_b, coll_c))
+
+    con_a = Contract(_total_amount=100.00, _client_id=1)
+    con_b = Contract(_total_amount=200.00, _client_id=1)
+    con_c = Contract(_total_amount=300.00, _client_id=2, _signed=True)
+    con_d = Contract(_total_amount=400.00, _client_id=2, _signed=True)
+    con_e = Contract(_total_amount=500.00, _client_id=4, _signed=True,
+                     _paid_amount=200.00)
+
+    uow.contracts = FakeRepository(
+        init=(con_a, con_b, con_c, con_d, con_e)
+    )
+
+    eve_a = Event(title="event_a", start_time=datetime(2025, 1, 1),
+                  end_time=datetime(2025, 1, 2), location="address a",
+                  attendee=30, notes="notes a", supporter_id=2, _contract_id=5)
+    eve_b = Event(_contract_id=4)
+    uow.events = FakeRepository(
+        init=(eve_a, eve_b)
+    )
+    return uow
+
+
+def test_create_event_success(init_uow):
+    data = {
+        "title": "test event !",
+        "contract_id": 3,
+        "useless_payload": "random data"
+    }
+    service = EventService(init_uow)
+    service.create(**data)
+    event_dto = service.retrieve(3)
+
+    assert event_dto.title == "test event !"
+    assert event_dto.start_time is None
+    assert event_dto.contract_id == 3
+
+
+def test_create_event_no_contract_fail(init_uow):
+    data = {
+        "title": "test event !"
+    }
+    service = EventService(init_uow)
+
+    with pytest.raises(EventServiceError,
+                       match="No contract found with id None"):
+        service.create(**data)
+
+
+def test_create_event_bad_contract_fail(init_uow):
+    data = {
+        "title": "test event !",
+        "contract_id": 9
+    }
+    service = EventService(init_uow)
+
+    with pytest.raises(EventServiceError, match="No contract found with id 9"):
+        service.create(**data)
+
+
+def test_create_event_contract_not_signed_fail(init_uow):
+    data = {
+        "title": "test event !",
+        "contract_id": 1
+    }
+    service = EventService(init_uow)
+
+    with pytest.raises(EventServiceError, match="Can't create event for "
+                                                "unsigned contracts"):
+        service.create(**data)
+
+
+def test_assign_support_success(init_uow):
+    service = EventService(init_uow)
+    service.assign_support(event_id=2, supporter_id=2)
+    event_dto = service.retrieve(2)
+
+    assert event_dto.supporter_id == 2
+
+
+def test_assign_support_bad_collaborator_fail(init_uow):
+    service = EventService(init_uow)
+    with pytest.raises(EventServiceError, match="Can't find collaborator"):
+        service.assign_support(event_id=2, supporter_id=10)
+
+
+def test_assign_support_collaborator_wrong_role_fail(init_uow):
+    service = EventService(init_uow)
+    with pytest.raises(EventServiceError,
+                       match="Can only assign supports to event"):
+        service.assign_support(event_id=2, supporter_id=1)
