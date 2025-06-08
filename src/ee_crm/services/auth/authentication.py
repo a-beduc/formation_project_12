6a09a85866e79821673e68f6bae8fc4c6fae35a8
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-06-08 23:43:23 +0200

    refactoring: no more pointless error wrapping

--- src/ee_crm/services/auth/authentication.py
+++ src/ee_crm/services/auth/authentication.py
@@ -13,19 +13,14 @@ class AuthenticationService:
     def verify_identity(uow, username, plain_password):
         user = uow.users.filter_one(username=username)
         if user is None:
-            raise AuthenticationError(f"User not found with {username}")
-
-        try:
-            user.verify_password(plain_password)
-        except AuthUserError:
-            raise AuthenticationError("Password mismatch")
-
+            raise AuthenticationError(f'No user found with username : '
+                                      f'"{username}"')
+        user.verify_password(plain_password)
         return user
 
     def authenticate(self, username, plain_password):
         with self.uow:
             user = self.verify_identity(self.uow, username, plain_password)
-
             collaborator = self.uow.collaborators.filter_one(user_id=user.id)
             return {
                 "sub": user.username,
