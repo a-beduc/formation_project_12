34ed6e06666c1b63a9649078a0826781bf4226ae
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-06-16 22:25:01 +0200

    feature: addition of CRUD commands for the CLI interface

---
+++ src/ee_crm/cli_interface/app/collaborator.py
@@ -0,0 +1,141 @@
+import click
+
+from ee_crm.cli_interface.views.view_collaborator import CollaboratorView
+from ee_crm.controllers.app.collaborator import CollaboratorManager
+from ee_crm.cli_interface.utils import (
+    clean_input_fields, normalize_fields, map_accepted_key,
+    normalize_remove_columns)
+from ee_crm.cli_interface.app.cli_func import (cli_prompt, cli_read,
+                                               cli_update, cli_delete)
+
+_EXPAND_ACCEPTED_KEYS = {
+    "id": {"id"},
+    "last_name": {"ln", "last name", "last_name"},
+    "first_name": {"fn", "first name", "first_name"},
+    "email": {"at", "email", "e_mail"},
+    "phone_number": {"ph", "phone number", "phone_number"},
+    "role": {"ro", "role"}
+}
+KEYS_MAP = map_accepted_key(_EXPAND_ACCEPTED_KEYS)
+
+PROMPT_CREATE = (
+    ("last_name", "string"),
+    ("first_name", "string"),
+    ("email", "string"),
+    ("phone_number", "20 char max"),
+    ("role", "3-5")
+)
+
+PROMPT_UPDATE = (
+    ("last_name", "string"),
+    ("first_name", "string"),
+    ("email", "string"),
+    ("phone_number", "20 char max")
+)
+
+
+@click.group()
+def collaborator():
+    pass
+
+
+@click.command()
+@click.option("-d", "--data-collaborator",
+              type=click.STRING,
+              nargs=2,
+              multiple=True,
+              help="optional data about collaborator")
+@click.option("-np", "--no-prompt", is_flag=True, default=False,
+              help="flag to turn off prompting to get additional data")
+def create(data_collaborator, no_prompt):
+    controller = CollaboratorManager()
+
+    username = click.prompt('create username')
+    password = click.prompt('create password (8 char, 1 upper, 1 lower, '
+                            '1 number)', hide_input=True)
+    password_2 = click.prompt('confirm password', hide_input=True)
+    if password != password_2:
+        click.echo('passwords do not match')
+        raise SystemExit(1)
+
+    cl_data = clean_input_fields(data_collaborator) or {}
+    norm_data = normalize_fields(cl_data, KEYS_MAP) or {}
+    comp_data = cli_prompt(norm_data, no_prompt, PROMPT_CREATE)
+
+    controller.create(username, password, **comp_data)
+
+    click.echo(f"User {username} successfully created")
+
+
+@click.command()
+@click.option("-pk", "-PK",
+              type=click.IntRange(min_open=1),
+              help="Collaborator's unique id, pk: INT >= 1")
+@click.option("-f", "--filter", "filters",
+              type=click.STRING,
+              nargs=2,
+              multiple=True,
+              help="KEY VALUE pair to apply a filter")
+@click.option("-s", "--sort", "sorts",
+              type=click.STRING,
+              multiple=True,
+              help="Ordered KEYs to apply a sort to the result of the query "
+                   "field:asc, field:desc")
+@click.option("-rc", "--remove-columns", "--remove-column",
+              type=click.STRING,
+              multiple=True,
+              help="Columns names to remove from result")
+def read(pk, filters, sorts, remove_columns):
+    output = cli_read(pk, filters, sorts, CollaboratorManager, KEYS_MAP)
+    remove_col = normalize_remove_columns(remove_columns, KEYS_MAP)
+    CollaboratorView().render(output, remove_col=remove_col)
+
+
+@click.command()
+@click.option("-pk", "-PK",
+              type=click.IntRange(min_open=1),
+              help="Collaborator's unique id, pk: INT >= 1")
+@click.option("-d", "--data-collaborator",
+              type=click.STRING,
+              nargs=2,
+              multiple=True,
+              help="optional data about collaborator")
+@click.option("-np", "--no-prompt", is_flag=True, default=False,
+              help="flag to turn off prompting to get additional data")
+def update(pk, data_collaborator, no_prompt):
+    cli_update(pk, data_collaborator, no_prompt, CollaboratorManager,
+               PROMPT_UPDATE, KEYS_MAP)
+    click.echo(f"Collaborator successfully updated")
+
+
+@click.command()
+@click.option("-pk", "-PK",
+              type=click.IntRange(min_open=1),
+              help="Collaborator's unique id, pk: INT >= 1")
+def delete(pk):
+    cli_delete(pk, CollaboratorManager)
+    click.echo(f"Collaborator successfully deleted")
+
+
+@click.command()
+@click.argument("pk",
+                type=click.IntRange(min_open=1))
+@click.argument("role",
+                type=click.STRING)
+def assign(pk, role):
+    """
+    Take two arguments, PK and ROLE.
+
+    PK : collaborator ID ;
+    ROLE : one of [DEACTIVATED, MANAGEMENT, SALES, SUPPORT]
+    """
+    controller = CollaboratorManager()
+    controller.change_collaborator_role(pk, role)
+    click.echo(f"Collaborator successfully assigned to new role : {role}")
+
+
+collaborator.add_command(create)
+collaborator.add_command(read)
+collaborator.add_command(update)
+collaborator.add_command(delete)
+collaborator.add_command(assign)
---
+++ src/ee_crm/cli_interface/app/event.py
@@ -0,0 +1,116 @@
+import click
+
+from ee_crm.cli_interface.views.view_event import EventView
+from ee_crm.controllers.app.event import EventManager
+from ee_crm.cli_interface.utils import map_accepted_key, \
+    normalize_remove_columns
+from ee_crm.cli_interface.app.cli_func import (cli_create, cli_read,
+                                               cli_update, cli_delete)
+
+
+_EXPAND_ACCEPTED_KEYS = {
+    "id": {"id"},
+    "title": {"ti", "title"},
+    "start_time": {"st", "start time", "start_time"},
+    "end_time": {"et", "end time", "end_time"},
+    "location": {"lo", "location"},
+    "attendee": {"at", "attendee"},
+    "notes": {"no", "notes"},
+    "supporter_id": {"su", "si", "supporter", "supporter id", "supporter_id"},
+    "contract_id": {"co", "ci", "contract", "contract id", "contract_id"}
+}
+KEYS_MAP = map_accepted_key(_EXPAND_ACCEPTED_KEYS)
+
+PROMPT_CREATE = (
+    ("title", "string"),
+    ("start_time", "string; format : 'YYYY-MM-DD HH:mm:ss'"),
+    ("end_time", "string; format : 'YYYY-MM-DD HH:mm:ss'"),
+    ("location", "string"),
+    ("attendee", "integer"),
+    ("notes", "string"),
+    ("contract_id", "integer")
+)
+
+PROMPT_UPDATE = (
+    ("title", "string"),
+    ("start_time", "string; format : 'YYYY-MM-DD HH:mm:ss'"),
+    ("end_time", "string; format : 'YYYY-MM-DD HH:mm:ss'"),
+    ("location", "string"),
+    ("attendee", "integer"),
+    ("notes", "string")
+)
+
+
+@click.group()
+def event():
+    pass
+
+
+@click.command()
+@click.option('-d', "--data-event",
+              type=click.STRING,
+              nargs=2,
+              multiple=True,
+              help="optional data about event")
+@click.option("-np", "--no-prompt", is_flag=True, default=False,
+              help="flag to turn off prompting to get additional data")
+def create(data_event, no_prompt):
+    cli_create(data_event, no_prompt, EventManager,
+               PROMPT_CREATE, KEYS_MAP)
+    click.echo("Event successfully created")
+
+
+@click.command()
+@click.option("-pk", "-PK",
+              type=click.IntRange(min_open=1),
+              help="Event's unique id, pk: INT >= 1")
+@click.option("-f", "--filters", "--filter",
+              type=click.STRING,
+              nargs=2,
+              multiple=True,
+              help="KEY VALUE pair to apply a filter")
+@click.option("-s", "--sorts", "--sort",
+              type=click.STRING,
+              multiple=True,
+              help="Ordered KEYs to apply a sort to the result of the query "
+                   "field:asc, field:desc")
+@click.option("-rc", "--remove-columns", "--remove-column",
+              type=click.STRING,
+              multiple=True,
+              help="Columns names to remove from result")
+def read(pk, filters, sorts, remove_columns):
+    output = cli_read(pk, filters, sorts, EventManager, KEYS_MAP)
+    remove_col = normalize_remove_columns(remove_columns, KEYS_MAP)
+    EventView().render(output, remove_col=remove_col)
+
+
+@click.command()
+@click.option("-pk", "-PK",
+              type=click.IntRange(min_open=1),
+              help="Event's unique id, pk: INT >= 1")
+@click.option("-d", "--data-event",
+              type=click.STRING,
+              nargs=2,
+              multiple=True,
+              help="optional data about collaborator")
+@click.option("-np", "--no-prompt", is_flag=True, default=False,
+              help="flag to turn off prompting to get additional data")
+def update(pk, data_event, no_prompt):
+    cli_update(pk, data_event, no_prompt, EventManager,
+               PROMPT_UPDATE, KEYS_MAP)
+    click.echo(f"Event successfully updated")
+
+
+@click.command()
+@click.option("-pk", "-PK",
+              type=click.IntRange(min_open=1),
+              help="Event's unique id, pk: INT >= 1")
+def delete(pk):
+    cli_delete(pk, EventManager)
+    click.echo(f"Event successfully deleted")
+
+
+event.add_command(create)
+event.add_command(read)
+event.add_command(update)
+event.add_command(delete)
