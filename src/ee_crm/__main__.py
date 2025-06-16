5a0bbcb430a166b887a5a91c10285a3a69ffc0cb
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-06-16 22:22:42 +0200

    feature: addition of entrypoint for the CLI commands

--- src/ee_crm/__main__.py
+++ src/ee_crm/__main__.py
@@ -2,6 +2,13 @@ from ee_crm.adapters.orm import start_mappers
 from ee_crm.cli_interface.entry_point import cli
 
 
-if __name__ == '__main__':
+def main():
     start_mappers()
-    cli()
+    try:
+        cli()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
--- src/ee_crm/cli_interface/entry_point.py
+++ src/ee_crm/cli_interface/entry_point.py
@@ -1,7 +1,11 @@
 import click
 
 from ee_crm.cli_interface.authentication import login, logout
-from ee_crm.cli_interface.collaborator import collaborator
+from ee_crm.cli_interface.app.collaborator import collaborator
+from ee_crm.cli_interface.app.client import client
+from ee_crm.cli_interface.app.contract import contract
+from ee_crm.cli_interface.app.event import event
+from ee_crm.cli_interface.app.user import user, who_am_i
 
 
 @click.group
@@ -11,4 +15,10 @@ def cli():
 
 cli.add_command(login)
 cli.add_command(logout)
+cli.add_command(who_am_i)
+
+cli.add_command(user)
 cli.add_command(collaborator)
+cli.add_command(client)
+cli.add_command(contract)
+cli.add_command(event)
