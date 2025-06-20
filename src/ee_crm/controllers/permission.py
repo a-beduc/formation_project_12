43ab42b20d342b4af16b172e8e54879fa7c10714
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-06-21 01:52:18 +0200

    images: images media to be used for readme.md

--- src/ee_crm/controllers/permission.py
+++ src/ee_crm/controllers/permission.py
@@ -9,6 +9,60 @@ from ee_crm.services.app.events import EventService
 from ee_crm.services.auth.jwt_handler import verify_token
 from ee_crm.services.unit_of_work import SqlAlchemyUnitOfWork
 
+# RBAC args decorator permission, ABAC using current predicate system after
+# if Role.DEACTIVATED ; ["ALL"]
+# if Role.SALES ; ["ALL", "COLLABORATOR", "SALES"]
+# ...
+# Flow idea :
+# get token role, use Role(token_role).name >>> "SALES"
+# "SALES" >>> perms in ["ALL", "COLLABORATOR", "SALES"]
+# in @permission, check if *args perms in list (RBAC)
+# then check if resources satisfy requirements predicate (old system) (ABAC)
+# proceed
+perms = {
+    "ALL": {
+        "user:modify_password_own",
+        "user:modify_username_own",
+    },
+    "COLLABORATOR": {
+        "collaborator:read",
+        "collaborator:update_self",
+        "collaborator:delete_self",
+        "client:read",
+        "contract:read",
+        "event:read"
+    },
+    "MANAGEMENT": {
+        "user:read",
+        "collaborator:create",
+        "collaborator:update_any",
+        "collaborator:delete_any",
+        "collaborator:modify_role",
+        "client:update_unassigned",
+        "client:delete_unassigned",
+        "contract:create",
+        "contract:update_unassigned",
+        "contract:delete_unassigned",
+        "event:modify_support",
+        "event:delete_unassigned"
+    },
+    "SALES": {
+        "client:create",
+        "client:update_own",
+        "client:delete_own",
+        "contract:delete_own",
+        "contract:sign_own",
+        "contract:modify_total_own",
+        "contract:pay_own",
+        "event:create",
+        "event:update_unassigned",
+        "event:delete_unassigned",
+    },
+    "SUPPORT": {
+        "event:update_own",
+    }
+}
+
 
 class P:
     """
