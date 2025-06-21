e540a6228e1c25c6cbb5f5d63654b96af1bf2d33
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-06-22 01:12:35 +0200

    feature: Implementation of a PermissionService that will handle request during permission check

---
+++ src/ee_crm/services/auth/permissions.py
@@ -0,0 +1,38 @@
+class PermissionService:
+    def __init__(self, uow):
+        self.uow = uow
+
+    def get_client_associated_salesman(self, client_id):
+        with self.uow:
+            try:
+                return self.uow.clients.get(client_id).salesman_id
+            except AttributeError:
+                return None
+
+    def get_contract_associated_salesman(self, contract_id):
+        with self.uow:
+            try:
+                return self.uow.contracts.get(contract_id).client.salesman_id
+            except AttributeError:
+                return None
+
+    def get_contract_signed(self, contract_id):
+        with self.uow:
+            try:
+                return self.uow.contracts.get(contract_id).signed
+            except AttributeError:
+                return None
+
+    def get_event_support(self, event_id):
+        with self.uow:
+            try:
+                return self.uow.events.get(event_id).supporter_id
+            except AttributeError:
+                return None
+
+    def get_event_associated_salesman(self, event_id):
+        with self.uow:
+            try:
+                return self.uow.events.get(event_id).contract.client.salesman_id
+            except AttributeError:
+                return None
