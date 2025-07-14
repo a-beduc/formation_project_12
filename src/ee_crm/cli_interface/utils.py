6149d1267255635698279255c87faca9e8d9c6a0
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-07-14 16:12:41 +0200

    tests: new tests for cli_interface/utils

--- src/ee_crm/cli_interface/utils.py
+++ src/ee_crm/cli_interface/utils.py
@@ -8,13 +8,13 @@ def map_accepted_key(accepted_keys):
 
 def clean_input_fields(fields):
     if not fields:
-        return
+        return None
     return {f[0].lower(): f[1] for f in fields}
 
 
 def clean_sort(sort):
     if not sort:
-        return
+        return None
     sort = [s.lower() for s in sort]
     output = []
     for s in sort:
@@ -29,19 +29,19 @@ def clean_sort(sort):
 
 def normalize_sort(sort, keys_map):
     if not sort:
-        return
+        return None
     return tuple([(keys_map.get(keyword, keyword), is_desc)
                   for keyword, is_desc in sort if keyword in keys_map])
 
 
 def normalize_fields(fields, keys_map):
     if not fields:
-        return
+        return None
     return {keys_map.get(k, k): v for k, v in fields.items() if k in keys_map}
 
 
 def normalize_remove_columns(columns, keys_map):
     if not columns:
-        return
+        return None
     return [keys_map[column] for column in columns if column in keys_map]
 
--- tests/test_cli_interface/test_utils.py
+++ tests/test_cli_interface/test_utils.py
@@ -1,5 +1,6 @@
 from ee_crm.cli_interface.utils import (
-    map_accepted_key, clean_sort, normalize_sort, normalize_fields)
+    map_accepted_key, clean_sort, normalize_sort, normalize_fields,
+    clean_input_fields, normalize_remove_columns)
 
 
 def test_map_accepted_key():
@@ -20,12 +21,29 @@ def test_map_accepted_key():
     assert map_accepted_key(accepted_key) == mapped_keys
 
 
+def test_clean_input_fields():
+    fields = [("TEST", "tEst"), ("vaLue", 51)]
+    cleaned_fields = clean_input_fields(fields)
+    assert cleaned_fields == {"test": "tEst", "value": 51}
+
+
+def test_clean_input_fields_empty():
+    fields = []
+    cleaned_fields = clean_input_fields(fields)
+    assert cleaned_fields is None
+
+
 def test_clean_sort():
     sort_in = ("data:expected", "unknown:asc", "FN:asc", "lASt_name",
                "role:dESc")
     sort_out = (("data:expected", False), ("unknown", False), ("fn", False),
                 ("last_name", False), ("role", True))
     assert clean_sort(sort_in) == sort_out
+
+
+def test_clean_sort_empty():
+    sort_in = ()
+    assert clean_sort(sort_in) is None
 
 
 def test_normalize_sort():
@@ -46,6 +64,22 @@ def test_normalize_sort():
     assert normalize_sort(sort_in, mapped_keys) == sort_out
 
 
+def test_normalize_sort_empty():
+    mapped_keys = {
+        "id": "id",
+        "ln": "last_name",
+        "last name": "last_name",
+        "last_name": "last_name",
+        "fn": "first_name",
+        "first name": "first_name",
+        "first_name": "first_name",
+        "rl": "role",
+        "role": "role"
+    }
+    sort_in = ()
+    assert normalize_sort(sort_in, mapped_keys) is None
+
+
 def test_normalize_fields():
     mapped_keys = {
         "id": "id",
@@ -60,3 +94,50 @@ def test_normalize_fields():
     fields_out = {"last_name": "Bobby", "first_name": "1568"}
     assert normalize_fields(fields_in, mapped_keys) == fields_out
 
+
+def test_normalize_fields_empty():
+    mapped_keys = {
+        "id": "id",
+        "ln": "last_name",
+        "last name": "last_name",
+        "last_name": "last_name",
+        "fn": "first_name",
+        "first name": "first_name",
+        "first_name": "first_name"
+    }
+    fields_in = {}
+    assert normalize_fields(fields_in, mapped_keys) is None
+
+
+def test_normalize_remove_columns():
+    columns_to_normalize = ["last_name", "fn", "unknown"]
+    mapped_keys = {
+        "id": "id",
+        "ln": "last_name",
+        "last name": "last_name",
+        "last_name": "last_name",
+        "fn": "first_name",
+        "first name": "first_name",
+        "first_name": "first_name"
+    }
+    columns_to_remove = normalize_remove_columns(
+        columns_to_normalize, mapped_keys)
+
+    assert columns_to_remove == ["last_name", "first_name"]
+
+
+def test_normalize_remove_columns_empty():
+    columns_to_normalize = []
+    mapped_keys = {
+        "id": "id",
+        "ln": "last_name",
+        "last name": "last_name",
+        "last_name": "last_name",
+        "fn": "first_name",
+        "first name": "first_name",
+        "first_name": "first_name"
+    }
+    columns_to_remove = normalize_remove_columns(
+        columns_to_normalize, mapped_keys)
+
+    assert columns_to_remove is None
