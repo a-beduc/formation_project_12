6b086c71d0df88b2855204c13e265e6976693af3
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-07-17 11:58:34 +0200

    docstring: DAL level

--- src/ee_crm/adapters/orm.py
+++ src/ee_crm/adapters/orm.py
@@ -1,5 +1,26 @@
-from sqlalchemy import (
-    Table, Column, Boolean, Integer, String, ForeignKey, DateTime, Float, Text)
+"""Map SQL tables to domain models using SQLAlchemy 2.x imperative
+mapping. We purposely use imperative (classical) mapping instead of
+declarative so that our entities does not depend on SQLAlchemy classes.
+
+Functions
+    start_user_mapper           # Map class 'AuthUser'
+    start_collaborator_mapper   # Map class 'Collaborator'
+    start_client_mapper         # Map class 'Client'
+    start_contract_mapper       # Map class 'Contract'
+    start_event_mapper          # Map class 'Event'
+    start_mappers               # Initialize all mappings
+
+References
+    * imperative mapping.
+https://docs.sqlalchemy.org/en/21/orm/mapping_styles.html#orm-imperative-mapping
+https://getdocs.org/Sqlalchemy/docs/latest/orm/mapping_styles#imperative-mapping-with-dataclasses-and-attrs
+    * synonym.
+https://docs.sqlalchemy.org/en/20/orm/mapped_attributes.html#codecell15
+    * column_property
+https://docs.sqlalchemy.org/en/21/orm/mapped_sql_expr.html#using-column-property
+"""
+from sqlalchemy import Table, Column, Boolean, Integer, String, ForeignKey, \
+    DateTime, Float, Text
 from sqlalchemy.orm import registry, relationship, synonym, column_property
 
 from ee_crm.domain.model import AuthUser, Collaborator, Client, Contract, Event
@@ -90,6 +111,17 @@ event_table = Table(
 
 
 def start_user_mapper():
+    """Map the AuthUser entity.
+
+    columns > attributes:
+        user_table.c.user_id    -> AuthUser.id
+        user_table.c.username   -> AuthUser._username
+        user_table.c.password   -> AuthUser._password
+
+    relationships:
+        'collaborator' back_populate create an attribute AuthUser.collaborator
+            -> (Collaborator)
+    """
     mapper_registry.map_imperatively(
         AuthUser,
         user_table,
@@ -107,6 +139,21 @@ def start_user_mapper():
 
 
 def start_collaborator_mapper():
+    """Map the Collaborator entity.
+
+    columns -> attributes:
+        collaborator_table.c.collaborator_id    -> Collaborator.id
+        collaborator_table.c.role_id            -> Collaborator._role_id
+        collaborator_table.c.user_id            -> Collaborator._user_id
+
+    relationships:
+        'user' back_populate create an attribute Collaborator.user
+            -> (AuthUser)
+        'clients' back_populate create an attribute Collaborator.clients
+            -> (list(Client))
+        'events' back_populate create an attribute Collaborator.events
+            -> (list(Event))
+    """
     mapper_registry.map_imperatively(
         Collaborator,
         collaborator_table,
@@ -134,6 +181,24 @@ def start_collaborator_mapper():
 
 
 def start_client_mapper():
+    """Map the Client entity.
+
+    columns -> attributes:
+        client_table.c.client_id    -> Client.id
+        client_table.c.created_at   -> Client._created_at
+        client_table.c.updated_at   -> Client._updated_at
+        client_table.c.salesman_id  -> Client._salesman_id
+
+    synonym:
+        Allows use of Client.salesman_id_sql in SQLAlchemy queries
+        even if mapped data is on attribute Client._salesman_id.
+
+    relationships:
+        'salesman' back_populate create an attribute Client.salesman
+            -> (Collaborator)
+        'contracts' back_populate create an attribute Client.contracts
+            -> (list(Contract))
+    """
     mapper_registry.map_imperatively(
         Client,
         client_table,
@@ -158,6 +223,29 @@ def start_client_mapper():
 
 
 def start_contract_mapper():
+    """Map the Contract entity.
+
+    columns -> attributes:
+        contract_table.c.contract_id    -> Contract.id
+        contract_table.c.total_amount   -> Contract._total_amount
+        contract_table.c.paid_amount    -> Contract._paid_amount
+        contract_table.c.signed         -> Contract._signed
+        contract_table.c.client_id      -> Contract._client_id
+
+    synonym:
+        Allows use of Contract.signed_sql in SQLAlchemy queries even if
+        mapped data is on attribute Client._signed.
+
+    column_property:
+        Allows use of a func due_amount_sql in SQLAlchemy queries to
+        interrogate a dynamically calculated value.
+
+    relationships:
+        'client' back_populate create an attribute Contract.client
+            -> (Client)
+        'event' back_populate create an attribute Contract.event
+            -> (Event)
+    """
     mapper_registry.map_imperatively(
         Contract,
         contract_table,
@@ -185,6 +273,18 @@ def start_contract_mapper():
 
 
 def start_event_mapper():
+    """Map the Event entity.
+
+    columns -> attributes:
+        event_table.c.event_id      -> Event.id
+        event_table.c.contract_id   -> Event._contract_id
+
+    relationships:
+        'contract' back_populate create an attribute Event.contract
+            -> (Contract)
+        'supporter' back_populate create an attribute Event.supporter
+            -> (Collaborator)
+    """
     mapper_registry.map_imperatively(
         Event,
         event_table,
@@ -206,6 +306,9 @@ def start_event_mapper():
 
 
 def start_mappers():
+    """Initialize the mappers. Need to be called when starting up the
+    application.
+    """
     start_user_mapper()
     start_collaborator_mapper()
     start_client_mapper()
