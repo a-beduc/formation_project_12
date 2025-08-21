1d27ba49a23e4e7c9bc8c22533d39d4c911f68f5
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-08-22 01:07:48 +0200

    docstrings: cli interface for collaborator

--- src/ee_crm/cli_interface/app/collaborator.py
+++ src/ee_crm/cli_interface/app/collaborator.py
@@ -1,3 +1,15 @@
+"""Click implementation of commands for the collaborator resource using
+Click.
+
+Functions:
+    collaborator    # click.group to organize commands under 'collaborator'
+    create          # Start the creation of a new collaborator and its
+                    # associated user
+    read            # Query database and display a table
+    update          # Update a specific collaborator
+    delete          # Delete a specific collaborator
+    assign_role     # Change a specific collaborator role
+"""
 import click
 
 from ee_crm.cli_interface.app.cli_func import cli_prompt, cli_read, \
@@ -36,20 +48,31 @@ PROMPT_UPDATE = (
 )
 
 
-@click.group()
+@click.group(help="Commands to manage collaborators.")
 def collaborator():
+    """Top level command group for collaborators."""
     pass
 
 
-@click.command()
+@click.command(help="Create a new collaborator and its user credentials.")
 @click.option("-d", "--data-collaborator",
               type=click.STRING,
               nargs=2,
               multiple=True,
-              help="optional data about collaborator")
+              help="Key-value pairs with collaborator data. "
+                   "(ex: --data-collaborator email user@mail.com)")
 @click.option("-np", "--no-prompt", is_flag=True, default=False,
               help="flag to turn off prompting to get additional data")
 def create(data_collaborator, no_prompt):
+    """Creates a new collaborator and a new user with mandatory username
+    and password. Display the new user information when transaction is
+    successful.
+
+    Args:
+        data_collaborator (iter(tuple[str, str])): the tuples contain
+            information about the new collaborator.
+        no_prompt (bool): block interactive prompt, false by default.
+    """
     controller = CollaboratorManager()
 
     username = click.prompt('create username')
@@ -70,7 +93,7 @@ def create(data_collaborator, no_prompt):
     viewer.render(output)
 
 
-@click.command()
+@click.command(help="Read collaborators from the database.")
 @click.option("-pk", "-PK",
               type=click.IntRange(min_open=1),
               help="Collaborator's unique id, pk: INT >= 1")
@@ -78,23 +101,37 @@ def create(data_collaborator, no_prompt):
               type=click.STRING,
               nargs=2,
               multiple=True,
-              help="KEY VALUE pair to apply a filter")
+              help="Key-value pairs to apply filters. "
+                   "(ex: --filter last_name Spring --filter role SUPPORT)")
 @click.option("-s", "--sort", "sorts",
               type=click.STRING,
               multiple=True,
-              help="Ordered KEYs to apply a sort to the result of the query "
-                   "field:asc, field:desc")
+              help="Keyword:direction to sort by one or more columns. "
+                   "(ex: --sort last_name:asc --sort id:desc)")
 @click.option("-rc", "--remove-columns", "--remove-column",
               type=click.STRING,
               multiple=True,
-              help="Columns names to remove from result")
+              help="Keyword to hide columns from output. "
+                   "(ex: --remove-column role)")
 def read(pk, filters, sorts, remove_columns):
+    """Queries collaborators and prints them in a formatted table.
+
+    Args:
+        pk (int): The unique ID of the collaborator.
+        filters (iter(tuple[str, str])): The tuples contain key-value
+            pairs where the key corresponds to the column name and value
+            is the filter to apply to the column.
+        sorts (tuple[str]): Ordered keyword to use for sorting.
+        remove_columns (tuple[str]): List of columns name to remove from
+            the table.
+    """
     output = cli_read(pk, filters, sorts, CollaboratorManager, KEYS_MAP)
     remove_col = normalize_remove_columns(remove_columns, KEYS_MAP)
     CollaboratorCrudView().render(output, remove_col=remove_col)
 
 
-@click.command()
+@click.command(help="Update a specific collaborators information in the "
+                    "database.")
 @click.option("-pk", "-PK",
               type=click.IntRange(min_open=1),
               help="Collaborator's unique id, pk: INT >= 1")
@@ -102,50 +139,76 @@ def read(pk, filters, sorts, remove_columns):
               type=click.STRING,
               nargs=2,
               multiple=True,
-              help="optional data about collaborator")
+              help="Key-value pairs with collaborator data. "
+                   "(ex: --data-collaborator email user@mail.com)")
 @click.option("-np", "--no-prompt", is_flag=True, default=False,
-              help="flag to turn off prompting to get additional data")
+              help="Disable interactive prompting for missing fields.")
 def update(pk, data_collaborator, no_prompt):
+    """Lookup for a specific collaborator and update its information
+    with the provided data.
+
+    Args:
+        pk (int): The unique ID of the collaborator.
+        data_collaborator (iter(tuple[str, str])): the tuples contain
+            information about the new collaborator.
+        no_prompt (bool): block interactive prompt, false by default.
+    """
     cli_update(pk, data_collaborator, no_prompt, CollaboratorManager,
                PROMPT_UPDATE, KEYS_MAP)
     CollaboratorCrudView().success(f"Collaborator successfully updated")
 
 
-@click.command()
+@click.command(help="Delete a specific collaborator and its associated "
+                    "user from the database.")
 @click.option("-pk", "-PK",
               type=click.IntRange(min_open=1),
               help="Collaborator's unique id, pk: INT >= 1")
 def delete(pk):
+    """Delete a specific collaborator from the database. It also delete
+    the associated user from the database.
+
+    Args:
+        pk (int): The unique ID of the collaborator.
+    """
     cli_delete(pk, CollaboratorManager)
     BaseView.success(f"Collaborator successfully deleted")
 
 
-@click.command()
+@click.command(help="Assign a new role to a collaborator. The roles must be"
+                    "a part of [DEACTIVATED, MANAGEMENT, SALES, SUPPORT]")
 @click.option("-pk", "-PK",
               type=click.IntRange(min_open=1),
               help="Collaborator's unique id, pk: INT >= 1")
 @click.option("-ro", "--role",
               type=click.Choice(
                   [
-                      1,
+                      "1",
                       "DEACTIVATED",
-                      3,
+                      "3",
                       "MANAGEMENT",
-                      4,
+                      "4",
                       "SALES",
-                      5,
+                      "5",
                       "SUPPORT"
                   ]
               ),
               nargs=1,
-              help="One of [DEACTIVATED, MANAGEMENT, SALES, SUPPORT]")
+              help="Role type to assign, one of "
+                   "[DEACTIVATED, MANAGEMENT, SALES, SUPPORT]")
 def assign_role(pk, role):
+    """
+
+    Args:
+        pk (int): The unique ID of the collaborator.
+        role (str): The new role to assign to the collaborator.
+    """
     controller = CollaboratorManager()
     controller.change_collaborator_role(pk, role)
     BaseView.success(
         f"Collaborator successfully assigned to new role : {role}")
 
 
+# Collaborator resource commands
 collaborator.add_command(create)
 collaborator.add_command(read)
 collaborator.add_command(update)
